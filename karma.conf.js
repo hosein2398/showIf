f7eddda7a8ef30c7498174556f1a35e65a810614
Hosein <hosein2398@gmail.com>
2017-08-27 22:44:18 +0330

    travis test again

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      './showif.js',
+      // './showif.js',
       './test/*.js'
     ],
 
@@ -29,7 +29,7 @@ module.exports = function(config) {
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
       'test/*.js': ['webpack'],
-      './showif.js' :['webpack']
+      // './showif.js' :['webpack']
       
     },
 
